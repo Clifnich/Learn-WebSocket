7c53b376388e2156bd20c77f06d1ba28bdcde6f7
Price Qian <puzhenq@gmail.com>
2020-02-10 17:29:54 -0800

    first commit

---
+++ src/main/java/Message.java
@@ -0,0 +1,11 @@
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Message {
+    private String from;
+    private String to;
+    private String content;
+}
+
